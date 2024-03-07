59975116a78d9526254c858adb589d61326d3a50
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-03-07 17:29:33 -0500

    Add project logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 // la estructura de carpetas cambia dependiendo el empaquetador
 
 const $app = document.getElementById('app');
-const API = 'https://api.esculajs.co/api/v1/products?offset=0&limit=10';
+const API = 'https://api.escuelajs.co/api/v1/products';
+
+const main = async () => {
+  const response = await fetch(API);
+  const products = await response.json();
+
+  const output = products?.map(product => {
+    return `
+      <article class="Card">
+        <img src="${product.images[0]}" alt="${product.title}" />
+        <h2>${product.title} <small>$ ${product.price}</small></h2>
+      </article>
+    `
+  }).join('');
+
+  let newItem = document.createElement('section');
+  newItem.classList.add('Items');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
+}
+
+main();
