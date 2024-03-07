1ce446c835744446f54f637cf8dd66c8e6d2ec6c
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-03-07 16:15:07 -0500

    Add project html structure and api url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 // la estructura de carpetas cambia dependiendo el empaquetador
+
+const $app = document.getElementById('app');
+const API = 'https://api.esculajs.co/api/v1/products?offset=0&limit=10';
